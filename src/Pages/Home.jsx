322e95eba489d4ca19e6a7c34850933819ac2846
Harischand2 <109259212+Harischand2@users.noreply.github.com>
2024-03-19 13:59:34 -0400

    remove unwanted code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import GreetingList from "../components/GreetingList";
 import GetStarted  from "../components/HomeBTN"; 
-import Footer from "../components/Footer";
 
 function Home() {
   return (
